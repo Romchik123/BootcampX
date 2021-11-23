1ef0644fd20aa8325fb8b947d350e91c9be928cf
rom <mozzieisme@gmail.com>
2021-11-23 16:39:16 -0500

    Add new data files to your DB and modify them

--- 4_queries/7.sql
+++ 4_queries/7.sql
@@ -1,8 +1,2 @@
-SELECT cohorts.name AS name,
-AVG(completed_at - started_at) AS average_assistance_time
+SELECT AVG(started_at - created_at) AS average_wait_time
 FROM assistance_requests
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-GROUP BY cohorts.name
-ORDER BY average_assistance_time DESC
-LIMIT 1;
